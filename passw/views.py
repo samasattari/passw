0b50ab174f8487bfb9251c38ae057b0dab5d2f96
samasattari <samasattari446@gmail.com>
2022-11-30 15:22:49 +0330

    'commitedfiled'

--- passw/views.py
+++ passw/views.py
@@ -1,12 +1,22 @@
 
 from django.shortcuts import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Password
 from .forms import PasswordForm
 
 
 def password_view(request):
-    form = PasswordForm()
+
+    # form = PasswordForm()
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+
+
+        if form.is_valid():
+            form.save()
+            
+            # return redirect("")
+
     password_list = Password.objects.all()
     context = {}
     context["passwords"] = password_list
